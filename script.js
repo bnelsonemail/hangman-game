50095ccf811c8422df70c7b05624aacdac7ab6df
bnelson <bnelsonemail@icloud.com>
2024-06-02 01:06:23 -0400

    project setup

---
+++ script.js
@@ -0,0 +1,17 @@
+/*              Game Requirements
+
+Description:  User will try to guess a hidden word by guessing letters one at a time.
+
+Requirements:
+1.  Display a seriese of blanks representing the hidden word.
+2.  Allow users to guess letters one at a time.
+3.  Keep track of which letters have been guessed.
+4.  Display the guessed letters and the blanks for the hidden word.
+5.  Display a win/loss message when the game is over.
+
+Optional:
+1.  First screen is a heading with a start button.
+2.  The heading and start button will disappear once the game starts.
+
+Working Code
+*/
